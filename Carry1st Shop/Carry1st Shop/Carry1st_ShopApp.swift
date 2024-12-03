cdcd488f94ff76414cedcb80e320b07d084b4f3e
GarbuJohnG <gabugashu@gmail.com>
2024-12-03 09:32:30 +0300

    App Setup

---
+++ Carry1st Shop/Carry1st Shop/Carry1st_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carry1st_ShopApp.swift
+//  Carry1st Shop
+//
+//  Created by John Gachuhi on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Carry1st_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carry1st Shop/Carry1st ShopTests/Carry1st_ShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  Carry1st_ShopTests.swift
+//  Carry1st ShopTests
+//
+//  Created by John Gachuhi on 03/12/2024.
+//
+
+import Testing
+@testable import Carry1st_Shop
+
+struct Carry1st_ShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
